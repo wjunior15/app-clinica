aca8bfa818a884c26a022780f3b16f49a981d133
Wellington A <wellingtonresende15@gmail.com>
2024-01-23 09:04:07 -0300

    add filtro de data na busca por consultas

--- create-db/paciente_consulta_data.sql
+++ create-db/paciente_consulta_data.sql
@@ -1,6 +1,7 @@
 SELECT funcionario.nome AS "medico", consulta.horas AS "horario", consulta.status_consulta AS "status", consulta.especialidade
 FROM consulta, funcionario
 WHERE consulta.ID_medico = funcionario.ID_func
+AND consulta.data_consulta = '2024-03-10'
 AND consulta.ID_paciente IN (SELECT ID_paciente
                              FROM paciente
                              WHERE nome LIKE '%John%')
@@ -9,6 +10,7 @@ SELECT funcionario.nome AS "medico", procedimento.horas AS "horario", procedimen
 FROM procedimento, funcionario, cirurgia
 WHERE procedimento.ID_medico = funcionario.ID_func
 AND procedimento.ID_procedimento = cirurgia.ID_procedimento
+AND procedimento.data_procedimento = '2024-03-10'
 AND procedimento.ID_paciente IN (SELECT ID_paciente
                              FROM paciente
                              WHERE nome LIKE '%John%')
@@ -17,6 +19,7 @@ SELECT funcionario.nome AS "medico", procedimento.horas AS "horario", procedimen
 FROM procedimento, funcionario, exame
 WHERE procedimento.ID_medico = funcionario.ID_func
 AND procedimento.ID_procedimento = exame.ID_procedimento
+AND procedimento.data_procedimento = '2024-03-10'
 AND procedimento.ID_paciente IN (SELECT ID_paciente
                              FROM paciente
                              WHERE nome LIKE '%John%')
