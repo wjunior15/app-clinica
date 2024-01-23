52a1e5df8236d2d06b67fcfcf6ca3d58a0e46060
Wellington A <wellingtonresende15@gmail.com>
2024-01-23 17:12:06 -0300

    add gitignore

--- app/source/rotas/rotas.py
+++ app/source/rotas/rotas.py
@@ -1,5 +1,5 @@
 import consultas.paciente_eventos as paciente_eventos
-import app.app as app
+import run.app as app
 
 @app.route('/paciente/<str:nome_paciente>/eventos/<str:data>')
 def eventos_paciente(nome_paciente, data):
