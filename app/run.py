fb9a61a86afcaba018b366954d667d1ba2bf503e
Wellington A <wellingtonresende15@gmail.com>
2024-01-23 23:57:47 -0300

    add consulta de receitas geradas por médico

--- app/run.py
+++ app/run.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import source.paciente_eventos as find_paciente_eventos
 import source.medico_eventos as find_medico_eventos
 import source.empresa_atendimentos as find_empresa_atendimentos
+import source.receitas_medico as find_receitas_medico
 app = Flask(__name__)
 
 if(__name__ == "__main__"):
@@ -38,4 +39,14 @@ if(__name__ == "__main__"):
             return jsonify(atendimentos), stt_code
         else:
             return jsonify({'status':'error'}), 404
+        
+    @app.route('/receitas/medico', methods=['GET'])
+    def receitas_medico():
+        dados = request.get_json()
+        if "crm" in dados:
+            receitas, stt_code = find_receitas_medico.receitas_medico(dados)
+            return jsonify(receitas), stt_code
+        else:
+            return jsonify({'status':'error'}), 404
+        
     app.run(debug=True, port=5000)
---
+++ app/source/receitas_medico.py
@@ -0,0 +1,35 @@
+import psycopg2
+import source.conn_db as conn_db
+from datetime import datetime
+
+def receitas_medico(dados):
+    crm = dados["crm"]
+    conn = conn_db.conn_db()
+    cur = conn.cursor()
+    cur.execute("""
+                    SELECT funcionario.nome AS "medico", paciente.nome AS "paciente", medicamento.nome AS "medicamento", medicamento.dose AS "dose", receita.data_receita AS "data"
+                    FROM funcionario, paciente, medicamento, receita, procedimento, remedio_receita
+                    WHERE procedimento.ID_receita = receita.ID_receita
+                    AND receita.ID_receita = remedio_receita.ID_receita
+                    AND remedio_receita.cod_remedio = medicamento.cod_remedio
+                    AND procedimento.ID_paciente = paciente.ID_paciente
+                    AND procedimento.ID_medico = funcionario.ID_func
+                    AND procedimento.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_buscado})
+                """.format(crm_buscado = crm))
+    rows = cur.fetchall()
+    conn_db.close_db(conn)
+    
+    if rows:
+        arr_receitas = []
+        for row in rows:
+            dict_receita = {
+                'medico':row[0],
+                'paciente':row[1],
+                'medicamento':row[2],
+                'dose':row[3],
+                'data':row[4].strftime("%d/%m/%Y")
+            }
+            arr_receitas.append(dict_receita)
+        return {'receitas':arr_receitas}, 200
+    else:
+        return {"erro":"Nenhuma receita encontrada"}, 404
