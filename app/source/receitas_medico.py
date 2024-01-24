7ce277991a163206cb848049d1984a81a9e2fce2
Wellington A <wellingtonresende15@gmail.com>
2024-01-24 00:34:23 -0300

    melhoria query busca receitas médicos

--- app/source/receitas_medico.py
+++ app/source/receitas_medico.py
@@ -14,8 +14,17 @@ def receitas_medico(dados):
                     AND remedio_receita.cod_remedio = medicamento.cod_remedio
                     AND procedimento.ID_paciente = paciente.ID_paciente
                     AND procedimento.ID_medico = funcionario.ID_func
-                    AND procedimento.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_buscado})
-                """.format(crm_buscado = crm))
+                    AND procedimento.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_busca})
+                    UNION
+                    SELECT funcionario.nome AS "medico", paciente.nome AS "paciente", medicamento.nome AS "medicamento", medicamento.dose AS "dose", receita.data_receita AS "data"
+                    FROM funcionario, paciente, medicamento, receita, consulta, remedio_receita
+                    WHERE consulta.ID_receita = receita.ID_receita
+                    AND receita.ID_receita = remedio_receita.ID_receita
+                    AND remedio_receita.cod_remedio = medicamento.cod_remedio
+                    AND consulta.ID_paciente = paciente.ID_paciente
+                    AND consulta.ID_medico = funcionario.ID_func
+                    AND consulta.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_busca})
+                """.format(crm_busca = crm))
     rows = cur.fetchall()
     conn_db.close_db(conn)
     
--- create-db/receitas_medico.sql
+++ create-db/receitas_medico.sql
@@ -5,4 +5,13 @@ AND receita.ID_receita = remedio_receita.ID_receita
 AND remedio_receita.cod_remedio = medicamento.cod_remedio
 AND procedimento.ID_paciente = paciente.ID_paciente
 AND procedimento.ID_medico = funcionario.ID_func
-AND procedimento.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = 67890)
+AND procedimento.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_busca})
+UNION
+SELECT funcionario.nome AS "medico", paciente.nome AS "paciente", medicamento.nome AS "medicamento", medicamento.dose AS "dose", receita.data_receita AS "data"
+FROM funcionario, paciente, medicamento, receita, consulta, remedio_receita
+WHERE consulta.ID_receita = receita.ID_receita
+AND receita.ID_receita = remedio_receita.ID_receita
+AND remedio_receita.cod_remedio = medicamento.cod_remedio
+AND consulta.ID_paciente = paciente.ID_paciente
+AND consulta.ID_medico = funcionario.ID_func
+AND consulta.ID_medico IN (SELECT ID_func FROM medico WHERE medico.crm = {crm_busca})
